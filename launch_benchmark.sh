a2fcaab17cf90c1cd9694844a23dc0dbbc8c012d
dujun <jun.du@intel.com>
2023-01-10 05:08:35 +0000

    update launch_benchmark.sh

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -9,7 +9,7 @@ function main {
     set_environment
 
     # requirements
-    pip install timm==0.4.5 tensorboardX six
+    pip install timm==0.4.5 tensorboardX six --no-deps
 
     # if multiple use 'xxx,xxx,xxx'
     model_name_list=($(echo "${model_name}" |sed 's/,/ /g'))
